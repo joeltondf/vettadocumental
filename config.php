8a1a93d9ef86885e8e2d7330e5d29363da7fbf26
joeltondf <joeltondf@gmail.com>
2025-10-04 11:03:37 -0300

    Ajusta fuso horario para America/Sao_Paulo

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+date_default_timezone_set('America/Sao_Paulo');
+
 // 1. Configurações de Conexão com a Base de Dados
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'u371107598_dadosnovalis');
