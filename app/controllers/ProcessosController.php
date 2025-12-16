861835fe2ed5b179228f52caa67247f6b3245064
testevta <joeltondf@gmail.com>
2025-12-16 16:54:24 -0300

    Ajustar data de conversao ao salvar OS

--- app/controllers/ProcessosController.php
+++ app/controllers/ProcessosController.php
@@ -2852,15 +2852,6 @@ class ProcessosController
             }
         }
 
-        if (!empty($dados['data_pagamento_1'])) {
-            $novoStatusNormalizado = $this->normalizeStatusName($novoStatus);
-            $servicoEmAndamentoNormalizado = $this->normalizeStatusName(ProcessStatus::SERVICE_IN_PROGRESS);
-
-            if ($novoStatusNormalizado === $servicoEmAndamentoNormalizado) {
-                $dados['data_conversao'] = $dados['data_pagamento_1'];
-            }
-        }
-
         return $dados;
     }
 
